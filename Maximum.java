870af817bc27ed8e909694986806def047c2a2d3
Roshan Gupta <roshangupta@Roshans-MacBook-Air.local>
2021-01-12 21:30:07 +0530

    uc2:Added function to find maximum between float

---
+++ Maximum.java
@@ -0,0 +1,30 @@
+public class Maximum {
+    public int findMax(Integer fValue, Integer sValue, Integer tValue) {
+
+        Integer max = fValue;
+
+        if(sValue.compareTo(max) > 0)
+        {
+            max = sValue;
+        }
+        if(tValue.compareTo(max) >0)
+        {
+            max = tValue;
+        }
+        return max;
+    }
+    public Float findMax(Float fValue, Float sValue, Float tValue) {
+
+        Float max = fValue;
+
+        if(sValue.compareTo(max) > 0)
+        {
+            max = sValue;
+        }
+        if(tValue.compareTo(max) >0)
+        {
+            max = tValue;
+        }
+        return max;
+    }
+}
